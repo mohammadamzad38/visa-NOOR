fe333b21a97747d46527ebebdadff9c4adb1a29d
Mohammad Amzad <hossainamzad38@gmail.com>
2025-12-07 18:24:57 +0600

    Some updates

--- visaCON-server/index.js
+++ visaCON-server/index.js
@@ -1,12 +1,14 @@
 const express = require("express");
 const app = express();
 const port = process.env.PORT || 5000;
+const morgan = require("morgan");
 const cors = require("cors");
 require("dotenv").config();
 const { MongoClient, ServerApiVersion } = require("mongodb");
 
 app.use(cors());
 app.use(express.json());
+app.use(morgan("dev"));
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@visacon.kw7svdu.mongodb.net/?appName=visaCON`;
 const client = new MongoClient(uri, {
